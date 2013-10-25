5f8cceab5402978680969c359772314599ca9616
chrisk3 <chris.kaundart@gmail.com>
2013-10-25 13:10:23 -0700

    initial upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BasicPractice::Application.config.secret_key_base = '327eadec32c33a1c9b1901e56638ae1e188a1855e2df020345116e73efd3a351085d639429366e6346d8156a982195505bf25ce894b43c1c363a4ccb27dbdcd8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BasicPractice::Application.config.session_store :cookie_store, key: '_basic_practice_session'
